16cb389cb4adfa790f6c3192eba494bf1a80a023
FabioFarinella96 <fabiofarinella96@gmail.com>
2023-03-13 18:37:42 +0100

    esercitazione del 13-03-2023

--- 13-03-2023/react-hooks/src/components/todoContainer/ToDoContainer.jsx
+++ 13-03-2023/react-hooks/src/components/todoContainer/ToDoContainer.jsx
@@ -1,11 +1,12 @@
 import "./index.css";
 import { useState } from "react";
 
-const ToDoContainer = ({ todo, setRemove }) => {
+const ToDoContainer = ({ todo }) => {
   const [checked, setChecked] = useState(false);
+  const [remove, setRemove] = useState(false);
 
   return (
-    <div className="ToDoContainer">
+    <div className={`ToDoContainer ${remove && "hidden-container"}`}>
       <p className="todo-text">{todo}</p>
 
       <button
@@ -13,6 +14,12 @@ const ToDoContainer = ({ todo, setRemove }) => {
         onClick={() => setChecked((prev) => !prev)}
       >
         {checked ? <p>Completed</p> : <p>Complete</p>}
+      </button>
+      <button
+        className="remove-button"
+        onClick={() => setRemove((prev) => !prev)}
+      >
+        X
       </button>
     </div>
   );
